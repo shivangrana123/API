75ee66a86f0bb1195bf1af3f6da0bbf40207462e
shivangrana123 <shivangrana123@gmail.com>
2021-04-01 22:42:52 +0530

    end of section 3

---
+++ DTos/UserDto.cs
@@ -0,0 +1,8 @@
+namespace API.DTos
+{
+    public class UserDto
+    {
+        public string Username { get; set; }
+        public string Token { get; set; }
+    }
+}
---
+++ Data/Migrations/20210319155036_InitialCreatenew.cs
@@ -0,0 +1,50 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace API.Data.Migrations
+{
+    public partial class InitialCreatenew : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropPrimaryKey(
+                name: "PK_MyProperty",
+                table: "MyProperty");
+
+            migrationBuilder.DropColumn(
+                name: "MyProperty",
+                table: "MyProperty");
+
+            migrationBuilder.RenameTable(
+                name: "MyProperty",
+                newName: "Users");
+
+            migrationBuilder.AddPrimaryKey(
+                name: "PK_Users",
+                table: "Users",
+                column: "Id");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropPrimaryKey(
+                name: "PK_Users",
+                table: "Users");
+
+            migrationBuilder.RenameTable(
+                name: "Users",
+                newName: "MyProperty");
+
+            migrationBuilder.AddColumn<int>(
+                name: "MyProperty",
+                table: "MyProperty",
+                type: "INTEGER",
+                nullable: false,
+                defaultValue: 0);
+
+            migrationBuilder.AddPrimaryKey(
+                name: "PK_MyProperty",
+                table: "MyProperty",
+                column: "Id");
+        }
+    }
+}
