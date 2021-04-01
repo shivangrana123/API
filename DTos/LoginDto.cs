238eb9f716598ac158a4744bf78ba66fdbc9ce83
shivangrana123 <shivangrana123@gmail.com>
2021-04-02 00:20:51 +0530

    added constraints

--- DTos/LoginDto.cs
+++ DTos/LoginDto.cs
@@ -2,7 +2,10 @@ namespace API.DTos
 {
     public class LoginDto
     {
+        [Required]
         public string Username { get; set; }
+        
+        [Required]
         public string Password { get; set; }
     }
 }
